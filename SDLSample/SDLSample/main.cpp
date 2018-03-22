6dc76b040c1dcb053d9b04abc2b4ccdaaf582046
文信周 <wenxinzhou@cvte.com>
2018-03-22 16:48:41 +0800

    显示图片

--- SDLSample/SDLSample/main.cpp
+++ SDLSample/SDLSample/main.cpp
@@ -1,12 +1,29 @@
 #include <windows.h>
 #include "SDL.h"
 
+void ShowPicture()
+{
+	auto screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+
+	auto picture = SDL_LoadBMP("helloworld.bmp");
+
+	SDL_BlitSurface(picture, nullptr, screen, nullptr);
+
+	SDL_Flip(screen);
+
+	SDL_Delay(10000);
+
+	SDL_FreeSurface(picture);
+}
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
+	ShowPicture();
+	
 	SDL_Quit();
 
 	return 0;
