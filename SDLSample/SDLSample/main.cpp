2323c2efc41c833d5f255ab313e35398879c4014
文信周 <wenxinzhou@cvte.com>
2018-03-27 17:06:00 +0800

    第5课 - 关键色

--- SDLSample/SDLSample/main.cpp
+++ SDLSample/SDLSample/main.cpp
@@ -23,6 +23,7 @@ const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
 
 SDL_Surface *g_bkImage;
+SDL_Surface *g_spriteImg;
 SDL_Surface *g_screen;
 
 SDL_Event g_event;
@@ -34,6 +35,12 @@ SDL_Surface* load_image(std::string path)
 	if (loadedImg != nullptr)
 	{
 		auto optimizedImg = SDL_DisplayFormat(loadedImg);
+
+		if (optimizedImg != nullptr)
+		{
+			auto colorKey = SDL_MapRGB(optimizedImg->format, 0, 0xFF, 0xFF);
+			SDL_SetColorKey(optimizedImg, SDL_SRCCOLORKEY, colorKey);
+		}
 
 		SDL_FreeSurface(loadedImg);
 
@@ -70,8 +77,12 @@ bool init()
 
 bool load_files()
 {
-	g_bkImage = load_image("look.png");
+	g_bkImage = load_image("background.png");
 	if (nullptr == g_bkImage)
+		return false;
+
+	g_spriteImg = load_image("foo.png");
+	if (nullptr == g_spriteImg)
 		return false;
 
 	return true;
@@ -80,6 +91,7 @@ bool load_files()
 void cleanup()
 {
 	SDL_FreeSurface(g_bkImage);
+	SDL_FreeSurface(g_spriteImg);
 
 	SDL_Quit();
 }
@@ -96,6 +108,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 		return -1;
 
 	apply_surface(0, 0, g_bkImage, g_screen);
+	apply_surface(240, 180, g_spriteImg, g_screen);
 
 	if (SDL_Flip(g_screen) == -1)
 		return -1;
